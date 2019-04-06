fc0f0b1b35e456d3840c91634f24080fd557726f
jfrost1610 <jobinthomas1610@gmail.com>
2019-04-06 22:34:39 +0530

    Spring Boot Base Setup

---
+++ src/main/java/com/frost/springboot/BootApp.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.frost.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jobin
+ *
+ */
+@SpringBootApplication
+public class BootApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(BootApp.class, args);
+
+	}
+
+}
