1e09a4c11e0ed0c427e7d23f7c933060895222a8
jfrost1610 <jobinthomas1610@gmail.com>
2019-04-06 23:45:47 +0530

    Webservice to create a new Topic added

--- src/main/java/com/frost/springboot/controller/TopicController.java
+++ src/main/java/com/frost/springboot/controller/TopicController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,11 @@ public class TopicController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void getTopic(@RequestBody Topic topic) {
+		topicService.createTopic(topic);
 	}
 
 }
--- src/main/java/com/frost/springboot/service/TopicService.java
+++ src/main/java/com/frost/springboot/service/TopicService.java
@@ -3,6 +3,7 @@
  */
 package com.frost.springboot.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,9 +18,10 @@ import com.frost.springboot.model.Topic;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("hibernate", "Hibernate Framework", "Hibernate Framework Description"),
-			new Topic("javascript", "JavaScript", "JavaScript Description"));
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
+					new Topic("hibernate", "Hibernate Framework", "Hibernate Framework Description"),
+					new Topic("javascript", "JavaScript", "JavaScript Description")));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -27,6 +29,10 @@ public class TopicService {
 
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void createTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
