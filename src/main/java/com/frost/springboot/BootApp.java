b9858383007cf3a0e6c1f4afc5b728ee3b9cadca
jfrost1610 <jobinthomas1610@gmail.com>
2019-04-06 23:11:56 +0530

    Updated spring parent version

--- src/main/java/com/frost/springboot/BootApp.java
+++ src/main/java/com/frost/springboot/BootApp.java
@@ -3,6 +3,7 @@
  */
 package com.frost.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -17,7 +18,6 @@ public class BootApp {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BootApp.class, args);
-
 	}
 
 }
